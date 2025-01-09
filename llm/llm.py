e5b1cdfeef5e7c950edf520c322d54527309f9b8
Evidlo <evan_copernicus@widloski.com>
2025-01-09 15:24:49 -0600

    add memory, use new mqtt api

--- llm/llm.py
+++ llm/llm.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import shelve
 import json
 import ollama
 from datetime import datetime
@@ -18,7 +19,8 @@ def llm_response(message: str):
     ).message.content
     return response
 
-def on_connect(client, userdata, flags, rc):
+#def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc, properties):
     """Connected to MQTT broker"""
     print(f"Connected with result code {rc}")
     client.subscribe("sms/received")
@@ -34,9 +36,28 @@ def on_message(client, userdata, msg):
     timestamp = datetime.fromtimestamp(int(msg['timestamp'][:-3]), UTC)
 
     # only respond if number is in our list
-    if number in numbers:
+    # or if message starts with 'llm '
+    if number in numbers or text.startswith('llm '):
+        text = text.removeprefix('llm ')
+        if number not in db:
+            db[number] = []
+
+        # store SMS content in history database
+        db[number].append({'role': 'user', 'content': text})
         # get a response from the LLM
-        response = llm_response(text)
+        # print(db[number][-10:])
+        response = ollama.chat(
+            model=model,
+            # use a single message for context
+            # messages=[{
+            #     'role': 'user',
+            #     'content': text
+            # }]
+            # use last N messages for context
+            messages=db[number][-10:]
+        ).message.content
+        # store response in history database
+        db[number].append({'role': 'assistant', 'content': response})
         # send response back to phone
         client.publish(
             "sms/send",
@@ -45,10 +66,14 @@ def on_message(client, userdata, msg):
                 'message': response
             })
         )
-        print(f"From {number} @ {timestamp}:\n    {text}")
-        print(f"    > {response}")
+        print(f"From {number} @ {timestamp}:\n    > {text}")
+        print(f"    {response}")
 
-client = mqtt.Client()
+# database for storing chat history
+db = shelve.open('history.sql', 'c', writeback=True)
+
+#client = mqtt.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 

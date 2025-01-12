33725134c125cd2d60747520305175f80fb7ff7c
evan <evan@evanw.org>
2025-01-11 20:08:30 -0600

    better log date format

--- android/app/src/main/java/com/ibnux/smsgatewaymqtt/Utils/Fungsi.java
+++ android/app/src/main/java/com/ibnux/smsgatewaymqtt/Utils/Fungsi.java
@@ -123,8 +123,13 @@ public class Fungsi {
         }
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(System.currentTimeMillis());
-        String newLog = cal.get(Calendar.YEAR) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DAY_OF_MONTH) + " " +
-                cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND)+"\n"+message;
+        String newLog = "-----" + String.format("%04d/%02d/%02d %02d:%02d:%02d", 
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH) + 1,
+                cal.get(Calendar.DAY_OF_MONTH),
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                cal.get(Calendar.SECOND)) + "-----\n" + message;
         writeToFile(newLog+"\n"+lastlog,file);
         BackgroundService.tellMainActivity();
     }
